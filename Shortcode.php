10ce242a7b232fe239a9623326adbca248f0af5a
gravitano <gravitano16@gmail.com>
2015-02-03 20:11:09 +0700

    Reformat code

--- Shortcode.php
+++ Shortcode.php
@@ -1,343 +1,357 @@
 <?php namespace Pingpong\Shortcode;
 
 use Countable;
-use Illuminate\Support\Str;
 use Illuminate\Container\Container;
+use Illuminate\Support\Str;
 
 class Shortcode implements Countable {
 
     /**
      * All registered shortcodes.
      *
-	 * @var array
-	 */
-	protected $shortcodes = []; 
+     * @var array
+     */
+    protected $shortcodes = [];
 
-	/**
-	 * The laravel container instance.
-	 * 
-	 * @var \Illuminate\Container\Container $container
-	 */
-	protected $container;
+    /**
+     * The laravel container instance.
+     *
+     * @var \Illuminate\Container\Container $container
+     */
+    protected $container;
 
-	/**
-	 * The constructor.
-	 * 
-	 * @param \Illuminate\Container\Container $container
-	 */
-	public function __construct(Container $container = null)
-	{
-		$this->container = $container ?: new Container;
-	}
+    /**
+     * The constructor.
+     *
+     * @param \Illuminate\Container\Container $container
+     */
+    public function __construct(Container $container = null)
+    {
+        $this->container = $container ?: new Container;
+    }
 
-	/**
-	 * Get the container instance.
-	 * 
-	 * @return \Illuminate\Container\Container
-	 */
-	public function getContainer()
-	{
-		return $this->container;
-	}
+    /**
+     * Get the container instance.
+     *
+     * @return \Illuminate\Container\Container
+     */
+    public function getContainer()
+    {
+        return $this->container;
+    }
 
-	/**
-	 * Set the laravel container instance.
-	 * 
-	 * @param \Illuminate\Container\Container $container
-	 * @return self
-	 */
-	public function setContainer(Container $container)
-	{
-		$this->container = $container;
-
-		return $this;
-	}
-
-	/**
-	 * Get all shortcodes.
-	 *
-	 * @return array
-	 */
-	public function all()
-	{
-		return $this->shortcodes;
-	}
-
-	/**
-	 * Register new shortcode.
-	 *
-	 * @param  string  $name
-	 * @param  mixed   $callback
-	 * @return void
-	 */
-	public function register($name, $callback)
-	{
-		$this->shortcodes[$name] = $callback;
-	}
-
-	/**
-	 * Unregister the specified shortcode by given name.
-	 *
-	 * @param  string  $name
-	 * @return void
-	 */
-	public function unregister($name)
-	{
-		if($this->exists($name))
-		{
-			unset($this->shortcodes[$name]);
-		}
-	}
-
-	/**
-	 * Unregister all shortcodes.
-	 *
+    /**
+     * Set the laravel container instance.
+     *
+     * @param \Illuminate\Container\Container $container
      * @return self
-	 */
-	public function destroy()
-	{
-		$this->shortcodes = [];
-        
+     */
+    public function setContainer(Container $container)
+    {
+        $this->container = $container;
+
         return $this;
-	}
+    }
 
-	/**
-	 * Get regex.
-	 *
-	 * @copyright Wordpress
-	 * @return string
-	 */
-	protected function getRegex()
-	{
-		$names = array_keys($this->shortcodes);
-        
+    /**
+     * Get all shortcodes.
+     *
+     * @return array
+     */
+    public function all()
+    {
+        return $this->shortcodes;
+    }
+
+    /**
+     * Register new shortcode.
+     *
+     * @param  string $name
+     * @param  mixed $callback
+     * @return void
+     */
+    public function register($name, $callback)
+    {
+        $this->shortcodes[$name] = $callback;
+    }
+
+    /**
+     * Unregister the specified shortcode by given name.
+     *
+     * @param  string $name
+     * @return void
+     */
+    public function unregister($name)
+    {
+        if ($this->exists($name))
+        {
+            unset($this->shortcodes[$name]);
+        }
+    }
+
+    /**
+     * Unregister all shortcodes.
+     *
+     * @return self
+     */
+    public function destroy()
+    {
+        $this->shortcodes = [];
+
+        return $this;
+    }
+
+    /**
+     * Get regex.
+     *
+     * @copyright Wordpress
+     * @return string
+     */
+    protected function getRegex()
+    {
+        $names = array_keys($this->shortcodes);
+
         $shortcode = join('|', array_map('preg_quote', $names));
-        
-        return
-			  '\\['
-			. '(\\[?)'
-			. "($shortcode)"
-			. '(?![\\w-])'
-			. '('
-			.     '[^\\]\\/]*'
-			.     '(?:'
-			.         '\\/(?!\\])'
-			.         '[^\\]\\/]*'
-			.     ')*?'
-			. ')'
-			. '(?:'
-			.     '(\\/)'
-			.     '\\]'
-			. '|'
-			.     '\\]'
-			.     '(?:'
-			.         '('
-			.             '[^\\[]*+'
-			.             '(?:'
-			.                 '\\[(?!\\/\\2\\])'
-			.                 '[^\\[]*+'
-			.             ')*+'
-			.         ')'
-			.         '\\[\\/\\2\\]'
-			.     ')?'
-			. ')'
-			. '(\\]?)';
-	}
 
-	/**
-	 * Parse string to attributes array.
-	 *
-	 * @return array
-	 */
-	protected function parseAttr($text)
-	{
+        return
+            '\\['
+            . '(\\[?)'
+            . "($shortcode)"
+            . '(?![\\w-])'
+            . '('
+            . '[^\\]\\/]*'
+            . '(?:'
+            . '\\/(?!\\])'
+            . '[^\\]\\/]*'
+            . ')*?'
+            . ')'
+            . '(?:'
+            . '(\\/)'
+            . '\\]'
+            . '|'
+            . '\\]'
+            . '(?:'
+            . '('
+            . '[^\\[]*+'
+            . '(?:'
+            . '\\[(?!\\/\\2\\])'
+            . '[^\\[]*+'
+            . ')*+'
+            . ')'
+            . '\\[\\/\\2\\]'
+            . ')?'
+            . ')'
+            . '(\\]?)';
+    }
+
+    /**
+     * Parse string to attributes array.
+     *
+     * @return array
+     */
+    protected function parseAttr($text)
+    {
         $atts = [];
 
-		$pattern = '/(\w+)\s*=\s*"([^"]*)"(?:\s|$)|(\w+)\s*=\s*\'([^\']*)\'(?:\s|$)|(\w+)\s*=\s*([^\s\'"]+)(?:\s|$)|"([^"]*)"(?:\s|$)|(\S+)(?:\s|$)/';
-        
+        $pattern = '/(\w+)\s*=\s*"([^"]*)"(?:\s|$)|(\w+)\s*=\s*\'([^\']*)\'(?:\s|$)|(\w+)\s*=\s*([^\s\'"]+)(?:\s|$)|"([^"]*)"(?:\s|$)|(\S+)(?:\s|$)/';
+
         $text = preg_replace("/[\x{00a0}\x{200b}]+/u", " ", $text);
-        
-        if ( preg_match_all($pattern, $text, $match, PREG_SET_ORDER) )
+
+        if (preg_match_all($pattern, $text, $match, PREG_SET_ORDER))
         {
             foreach ($match as $m)
             {
-				if (!empty($m[1]))
-					$atts[strtolower($m[1])] = stripcslashes($m[2]);
-				elseif (!empty($m[3]))
-					$atts[strtolower($m[3])] = stripcslashes($m[4]);
-				elseif (!empty($m[5]))
-					$atts[strtolower($m[5])] = stripcslashes($m[6]);
-				elseif (isset($m[7]) and strlen($m[7]))
-					$atts[] = stripcslashes($m[7]);
-				elseif (isset($m[8]))
-					$atts[] = stripcslashes($m[8]);
-			}
+                if ( ! empty($m[1]))
+                {
+                    $atts[strtolower($m[1])] = stripcslashes($m[2]);
+                }
+                elseif ( ! empty($m[3]))
+                {
+                    $atts[strtolower($m[3])] = stripcslashes($m[4]);
+                }
+                elseif ( ! empty($m[5]))
+                {
+                    $atts[strtolower($m[5])] = stripcslashes($m[6]);
+                }
+                elseif (isset($m[7]) and strlen($m[7]))
+                {
+                    $atts[] = stripcslashes($m[7]);
+                }
+                elseif (isset($m[8]))
+                {
+                    $atts[] = stripcslashes($m[8]);
+                }
+            }
         }
         else
         {
-			$atts = ltrim($text);
+            $atts = ltrim($text);
         }
 
-		return $atts;
-	}
+        return $atts;
+    }
 
-	/**
-	 * Strip any shortcodes.
-	 *
-	 * @param  string $content
-	 * @return string
-	 */
-	public function strip($content)
-	{
-		if (empty($this->shortcodes)) return $content;
+    /**
+     * Strip any shortcodes.
+     *
+     * @param  string $content
+     * @return string
+     */
+    public function strip($content)
+    {
+        if (empty($this->shortcodes)) return $content;
 
-		$pattern = $this->getRegex();
+        $pattern = $this->getRegex();
 
-		return preg_replace_callback("/{$pattern}/s", function($m)
-		{
-			if ($m[1] == '[' && $m[6] == ']')
-			{
-				return substr($m[0], 1, -1);
-			}
+        return preg_replace_callback("/{$pattern}/s", function ($m)
+        {
+            if ($m[1] == '[' && $m[6] == ']')
+            {
+                return substr($m[0], 1, -1);
+            }
 
-			return $m[1] . $m[6];
+            return $m[1] . $m[6];
         }, $content);
-	}
+    }
 
-	/**
-	 * Get count from all shortcodes.
-	 *
-	 * @return integer
-	 */
-	public function count()
-	{
-		return count($this->shortcodes);
-	}
+    /**
+     * Get count from all shortcodes.
+     *
+     * @return integer
+     */
+    public function count()
+    {
+        return count($this->shortcodes);
+    }
 
-	/**
-	 * Return true is the given name exist in shortcodes array.
-	 *
-	 * @param  string  $name
-	 * @return boolean
-	 */
-	public function exists($name)
-	{
-		return array_key_exists($name, $this->shortcodes);
-	}
+    /**
+     * Return true is the given name exist in shortcodes array.
+     *
+     * @param  string $name
+     * @return boolean
+     */
+    public function exists($name)
+    {
+        return array_key_exists($name, $this->shortcodes);
+    }
 
-	/**
-	 * Return true is the given content contain the given name shortcode.
-	 *
-	 * @param  string  $content
-	 * @param  string  $name
-	 * @return boolean
-	 */
-	public function contains($content, $name)
-	{
+    /**
+     * Return true is the given content contain the given name shortcode.
+     *
+     * @param  string $content
+     * @param  string $name
+     * @return boolean
+     */
+    public function contains($content, $name)
+    {
         if ($this->exists($name))
         {
-			preg_match_all( '/' . $this->getRegex() . '/s', $content, $matches, PREG_SET_ORDER );
-            
+            preg_match_all('/' . $this->getRegex() . '/s', $content, $matches, PREG_SET_ORDER);
+
             if (empty($matches))
-				return false;
+            {
+                return false;
+            }
 
             foreach ($matches as $shortcode)
             {
-				if ($name === $shortcode[2])
-					return true;
-			}
+                if ($name === $shortcode[2])
+                {
+                    return true;
+                }
+            }
         }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * Compile the gived content.
-	 *
-	 * @param  string  $content
-	 * @return void
-	 */
-	public function compile($content)
-	{
-		if( ! $this->count()) return $content;
-        
+    /**
+     * Compile the gived content.
+     *
+     * @param  string $content
+     * @return void
+     */
+    public function compile($content)
+    {
+        if ( ! $this->count()) return $content;
+
         $pattern = $this->getRegex();
 
-		return preg_replace_callback("/{$pattern}/s", [&$this, 'render'], $content);
-	}
+        return preg_replace_callback("/{$pattern}/s", [&$this, 'render'], $content);
+    }
 
-	/**
-	 * Render the current calld shortcode.
-	 *
-	 * @param  array  $matches
-	 * @return void
-	 */
-	public function render($matches)
-	{
-		$name 		= array_get($matches, 2);
-        
-        $content 	= array_get($matches, 5);
-        
-        $callback 	= $this->getCallback($name);
-        
-        $params		= $this->getParameter($matches);
-        
-        $params 	= [$params, $content, $name];
-        
+    /**
+     * Render the current calld shortcode.
+     *
+     * @param  array $matches
+     * @return void
+     */
+    public function render($matches)
+    {
+        $name = array_get($matches, 2);
+
+        $content = array_get($matches, 5);
+
+        $callback = $this->getCallback($name);
+
+        $params = $this->getParameter($matches);
+
+        $params = [$params, $content, $name];
+
         return call_user_func_array($callback, $params);
-	}
+    }
 
-	/**
-	 * Get parameters.
-	 *
-	 * @param  array $matches
-	 * @return array
-	 */
-	protected function getParameter($matches)
-	{
-		$params = $this->parseAttr($matches[3]);
-        
-        if( ! is_array($params))
-		{
-			$params = [$params];
+    /**
+     * Get parameters.
+     *
+     * @param  array $matches
+     * @return array
+     */
+    protected function getParameter($matches)
+    {
+        $params = $this->parseAttr($matches[3]);
+
+        if ( ! is_array($params))
+        {
+            $params = [$params];
         }
 
-		return $params;
-	}
+        return $params;
+    }
 
-	/**
-	 * Get callback from given shortcode name.
-	 *
-	 * @param  string  $name
-	 * @return mixed
-	 */
-	protected function getCallback($name)
-	{
-		$callback = $this->shortcodes[$name];
-        
-        if(is_string($callback))
-		{
-			if(Str::contains($callback, '@'))
-			{
-				$parsedCallback = Str::parseCallback($callback, 'register');
-				
-				$instance = $this->container->make($parsedCallback[0]);
-                
+    /**
+     * Get callback from given shortcode name.
+     *
+     * @param  string $name
+     * @return mixed
+     */
+    protected function getCallback($name)
+    {
+        $callback = $this->shortcodes[$name];
+
+        if (is_string($callback))
+        {
+            if (Str::contains($callback, '@'))
+            {
+                $parsedCallback = Str::parseCallback($callback, 'register');
+
+                $instance = $this->container->make($parsedCallback[0]);
+
                 return [$instance, $parsedCallback[1]];
-			}
-			elseif(class_exists($callback))
-			{
-				$instance = $this->container->make($callback);
+            }
+            elseif (class_exists($callback))
+            {
+                $instance = $this->container->make($callback);
 
-				return [$instance, 'register'];
+                return [$instance, 'register'];
             }
             else
-			{
-				return $callback;
-			}
+            {
+                return $callback;
+            }
         }
 
-		return $callback;
-	}
+        return $callback;
+    }
 
 }
--- ShortcodeServiceProvider.php
+++ ShortcodeServiceProvider.php
@@ -4,44 +4,44 @@ use Illuminate\Support\ServiceProvider;
 
 class ShortcodeServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('pingpong/shortcode');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('pingpong/shortcode');
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['shortcode'] = $this->app->share(function($app)
-		{
-			return new Shortcode;
-		});
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['shortcode'] = $this->app->share(function ($app)
+        {
+            return new Shortcode;
+        });
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('shortcode');
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('shortcode');
+    }
 
 }
